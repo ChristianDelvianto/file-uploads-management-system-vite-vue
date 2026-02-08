2492aea4a77785dad83e1ddb15fff64971fd9110
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2026-02-08 11:30:06 +0700

    add vite configuration and remote default built-in components

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    tailwindcss(),
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, './src'),
+    },
+    extensions: ['.js', '.ts', '.json']
+  }
 })
