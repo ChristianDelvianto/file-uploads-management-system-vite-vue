8bb12066b9eeff1d71c2f5a26e1901df3b9622dc
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2026-02-21 18:31:43 +0700

    remove classes line breaks in Button.vue

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -24,32 +24,14 @@ const borderColor = {
     'primary': 'border-blue-600',
 }[props.level]
 const focusEffect = {
-    'danger': `
-        focus:bg-red-900
-        focus:outline-red-300
-    `,
-    'neutral': `
-        focus:border-blue-600
-        focus:text-blue-600
-        focus:outline-blue-300
-    `,
-    'primary': `
-        focus:bg-blue-900
-        focus:outline-blue-300
-    `,
+    'danger': 'focus:bg-red-900 focus:outline-red-300',
+    'neutral': 'focus:border-blue-600 focus:text-blue-600 focus:outline-blue-300',
+    'primary': 'focus:bg-blue-900 focus:outline-blue-300',
 }[props.level]
 const hoverEffect = {
-    'danger': `
-        hover:bg-red-900
-    `,
-    'neutral': `
-        hover:bg-blue-100
-        hover:border-blue-600
-        hover:text-blue-600
-    `,
-    'primary': `
-         hover:bg-blue-900
-    `,
+    'danger': 'hover:bg-red-900',
+    'neutral': 'hover:bg-blue-100 hover:border-blue-600 hover:text-blue-600',
+    'primary': 'hover:bg-blue-900',
 }[props.level]
 const textColor = {
     'danger': 'text-white',
