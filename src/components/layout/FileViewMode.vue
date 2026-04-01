e9290d14e81b022704339b5c8e5390f777487377
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2026-04-01 17:42:20 +0700

    add FileViewMode component for toggleable list and grid views

---
+++ src/components/layout/FileViewMode.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import IconMDIFormatListBulletedSquare from '@src/components/svg/mdi/FormatListBulletedSquare.vue'
+import IconMDIViewGridOutline from '@src/components/svg/mdi/ViewGridOutline.vue'
+import { ViewMode as FileViewMode } from '@src/stores/modules/file/types'
+
+defineEmits<{
+    (eventName: 'update:view-mode', viewMode: FileViewMode): void
+}>()
+
+withDefaults(defineProps<{
+    viewMode: FileViewMode,
+}>(), {
+    viewMode: 'grid',
+})
+</script>
+
+<template>
+    <div
+        class="flex flex-grow-0 flex-row flex-shrink items-center"
+    >
+        <!-- List mode -->
+        <button
+            @click="$emit('update:view-mode', 'list')"
+            :class="{
+                'bg-white border-stone-300 z-[0]': viewMode === 'grid',
+                'bg-blue-300/60 border-blue-600/60 z-[1]': viewMode === 'list',
+            }"
+            class="border flex-grow-0 flex-shrink-0 -mr-[1px] px-3 py-1 relative rounded-l-full"
+        >
+            <IconMDIFormatListBulletedSquare
+                :color="viewMode === 'list' ? '#1D4ED8' : undefined"
+            />
+        </button>
+
+        <!-- Grid mode -->
+        <button
+            @click="$emit('update:view-mode', 'grid')"
+            :class="{
+                'bg-blue-300/60 border-blue-600/60 z-[1]': viewMode === 'grid',
+                'bg-white border-stone-300 z-[0]': viewMode === 'list',
+            }"
+            class="border flex-grow-0 flex-shrink-0 px-3 py-1 relative rounded-r-full"
+        >
+            <IconMDIViewGridOutline
+                :color="viewMode === 'grid' ? '#1D4ED8' : undefined"
+            />
+        </button>
+    </div>
+</template>
