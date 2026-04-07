eea65ead6129df46f5a236e04e6fb9824a67ee4f
ChristianDelvianto <github.placeholder.delvin@gmail.com>
2026-04-07 13:55:38 +0700

    add svg icons

---
+++ src/components/svg/mdi/File.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+withDefaults(defineProps<Readonly<{
+    color?: string,
+    size?: number,
+}>>(), {
+    color: '#4A5565', // text-gray-600
+    size: 24,
+})
+</script>
+
+<template>
+    <svg
+        :height="$props.size"
+        :width="$props.size"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            :fill="$props.color"
+            d="M13 9V3.5L18.5 9M6 2c-1.11 0-2 .89-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6z"
+        />
+    </svg>
+</template>
---
+++ src/components/svg/mdi/Public.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+withDefaults(defineProps<Readonly<{
+    color?: string,
+    size?: number,
+}>>(), {
+    color: '#4A5565', // text-gray-600
+    size: 24,
+})
+</script>
+
+<template>
+    <svg
+        :height="$props.size"
+        :width="$props.size"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            :fill="$props.color"
+            d="M17.9 17.39c-.26-.8-1.01-1.39-1.9-1.39h-1v-3a1 1 0 0 0-1-1H8v-2h2a1 1 0 0 0 1-1V7h2a2 2 0 0 0 2-2v-.41a7.984 7.984 0 0 1 2.9 12.8M11 19.93c-3.95-.49-7-3.85-7-7.93c0-.62.08-1.22.21-1.79L9 15v1a2 2 0 0 0 2 2m1-16A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2"
+        />
+    </svg>
+</template>
