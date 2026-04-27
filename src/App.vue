5f3707df77abafc16a6903315fc7f4c164608170
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-04-27 18:02:11 +0700

    refactor App component and improve error logic

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,35 @@
 <script setup lang="ts">
 import PageError from '@src/components/page/PageError.vue'
 import PageLoading from '@src/components/page/PageLoading.vue'
+import { useAuth } from '@src/composables/useAuth'
 import { usePage } from '@src/composables/usePage'
 import { useStore } from 'vuex'
 import { useRoute, useRouter } from 'vue-router'
 import { onBeforeMount } from 'vue'
+import { isAxiosError } from 'axios'
 const store = useStore()
 const route = useRoute()
 const router = useRouter()
-const { errorCode, isError, isLoading, pageIsError, pageLoading } = usePage()
+const { role } = useAuth()
+const { isPageError, isPageLoading, pageErrorCode, pageIsError, pageLoading } = usePage()
 
-async function handleCheckAuthError(err: any): Promise<void> {
+async function handleCheckAuthError(err: unknown): Promise<void> {
   console.error('Error checking authentication:', err)
 
-  const errCode = err?.response?.status ?? 0
+  let errCode = 0
   
-  if (errCode === 401) {
-    // Redirect to login
-    if (route.meta.userOnly) {
-      await router.push('/login')
+  if (isAxiosError(err) && err.response?.status) {
+    const statusCode = err.response.status
+
+    if (statusCode === 401) {
+      if (route.meta.userOnly) {
+        await router.push('/login')
+      }
+
+      return
     }
 
-    return
+    errCode = statusCode
   }
   
   await pageIsError(errCode)
@@ -30,31 +38,31 @@ async function checkAuth(): Promise<void> {
   pageLoading(true)
 
   try {
-    const { role } = await store.dispatch('auth/check')
+    await store.dispatch('auth/check')
 
-    if (route.meta.guestOnly || route.meta.role && route.meta.role !== role) {
+    if (route.meta.guestOnly || route.meta.role && route.meta.role !== role.value) {
       await router.replace({
-        name: `${role}.dashboard`,
+        name: `${role.value}.dashboard`,
       })
     }
-  } catch (err) {
+  } catch (err: unknown) {
     await handleCheckAuthError(err)
   } finally {
     pageLoading(false)
   }
 }
 
-onBeforeMount((): void => {
+onBeforeMount(function (): void {
   checkAuth()
 })
 </script>
 
 <template>
-  <PageLoading v-if="isLoading" class="min-h-screen" />
+  <PageLoading v-if="isPageLoading" class="min-h-screen" />
   <PageError
-    v-else-if="isError"
+    v-else-if="isPageError"
     @retry="checkAuth"
-    :error-code="errorCode"
+    :error-code="pageErrorCode"
     class="min-h-screen"
   />
   <RouterView v-else />
