38e083c5d7d0d636ff5af9deb80b6868aea2d8f5
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-08-08 19:01:51 +0700

    add FileCardOwner component

---
+++ src/components/file/FileCardOwner.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import UserAvatar from '@/components/user/Avatar.vue'
+import { useAuth } from '@/composables/useAuth'
+import { User } from '@/types/user'
+const { profile } = useAuth()
+
+defineProps<Readonly<{
+    owner: User | null
+}>>()
+</script>
+
+<template>
+    <div
+        class="flex-grow flex-row flex-shrink gap-1.5 items-center hidden
+        lg:flex lg:w-1/3"
+    >
+        <UserAvatar
+            :icon-size="16"
+            :photo="owner?.photo || null"
+            class="size-8"
+        />
+
+        {{
+            owner?.id === profile?.id
+                ? 'me'
+                : owner?.name
+        }}
+    </div>
+</template>
