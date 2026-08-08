de0292f434f5cf491aaa2df513b5f2c1d5fe8b43
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-08-08 18:45:00 +0700

    refactor useAuth to enhance type safety and improve computed properties

--- src/composables/useAuth.ts
+++ src/composables/useAuth.ts
@@ -1,16 +1,32 @@
+import { PlanUser } from '@/types/plan'
+import { User } from '@/types/user'
 import { useStore } from 'vuex'
 import { computed } from 'vue'
 
 export const useAuth = () => {
     const store = useStore()
 
-    const authenticated = computed(() => store.getters['auth/authenticated'])
-    const profile = computed(() => store.getters['auth/profile'])
-    const role = computed(() => store.getters['auth/role'])
+    const authenticated = computed<boolean>(() => {
+        return store.getters['auth/authenticated']
+    })
+    const profile = computed<User | null>(() => {
+        return store.getters['auth/profile']
+    })
+    const usedBytes = computed<number>(() => {
+        return store.getters['auth/used_bytes']
+    })
+    const userPlan = computed<PlanUser>(() => {
+        return {
+            id: store.getters['auth/plan_id'],
+            limit_bytes: store.getters['auth/plan_limit_bytes'],
+            name: store.getters['auth/plan_name']
+        }
+    })
 
     return {
         authenticated,
         profile,
-        role,
+        usedBytes,
+        userPlan
     }
 }
