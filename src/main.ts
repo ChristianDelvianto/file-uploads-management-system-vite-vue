7e6403aab3a91ae3f5041dee607f9e57ee93cebb
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-08-08 18:50:22 +0700

    refactor main.ts to improve API configuration and enhance readability

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,31 @@ import router from './router'
 import Api from './services'
 import App from './App.vue'
 
-// Set Api's base URL
-Api.defaults.baseURL = import.meta.env.VITE_API_BASE_URL ?? '/api/v1'
+declare global {
+    const APP_NAME: string;
+    const APP_VERSION: string
+}
+
+// Set API default base URL
+Api.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || '/api/v1'
+Api.defaults.timeout = 60_000
 
 const vue = createApp(App)
 vue.use(store)
 vue.use(router)
 
+vue.config.globalProperties.$APP_NAME = APP_NAME
+vue.config.globalProperties.$APP_VERSION = APP_VERSION
+
 router.isReady()
-    .then(function (): void {
-        // Set initial width
+.then((): void => {
+    // Set initial width
+    store.commit('window/SET_WIDTH', window.innerWidth)
+
+    // When user resizing the window, store window's inner width
+    window.addEventListener('resize', (): void => {
         store.commit('window/SET_WIDTH', window.innerWidth)
-
-        // When user resizing the window, store window inner width
-        window.addEventListener('resize', (): void => {
-            store.commit('window/SET_WIDTH', window.innerWidth)
-        })
-
-        vue.mount('#app')
     })
+
+    vue.mount('#app')
+})
