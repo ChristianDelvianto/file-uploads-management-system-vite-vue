2824cce8b0a71658e20fb650d75e5ad14196975d
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-08-08 18:53:16 +0700

    refactor PageError component for improved error handling and readability

--- src/components/page/PageError.vue
+++ src/components/page/PageError.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import Button from '@src/components/ui/Button.vue'
-import IconMDIErrorOutline from '@src/components/svg/mdi/ErrorOutline.vue'
+import Button from '@/components/ui/Button.vue'
+import IconMDIErrorOutline from '@/components/svg/mdi/ErrorOutline.vue'
 import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
 const store = useStore()
@@ -11,18 +11,22 @@ defineEmits<{
 }>()
 
 const props = withDefaults(defineProps<Readonly<{
-    errorCode: number
+    errorCode: number,
+    errorImage?: string,
+    errorMessage?: string
 }>>(), {
     errorCode: 0
 })
 
-const message = {
-    0: 'Sorry, something went wrong, please try again', // Default
+const errorMessage = {
+    0: 'Something went wrong, please try again or refresh the page', // Default
     400: 'Bad request',
-    401: 'It seems that you are not logged in, please log in to access to continue',
+    401: 'It seems that you are not logged in, please log in to continue',
     403: 'Sorry, you are not allowed to access this',
     404: 'Opps, sorry but page not found',
-    500: 'Internal server error, please try again'
+    429: 'Too many requests, please slow down and try again later',
+    500: 'Internal server error, please try again',
+    503: 'Service unavailable, please try again later'
 }[props.errorCode]
 
 async function toLoginPage(): Promise<void> {
@@ -31,29 +35,45 @@ async function toLoginPage(): Promise<void> {
     await router.push('/login')
         .then(() => {
             store.dispatch('auth/logout')
+
+            store.dispatch('upload/removeAll')
         })
 }
 </script>
 
 <template>
-    <div class="flex flex-col gap-y-3 items-center justify-center p-6">
-        <IconMDIErrorOutline :size="64" />
+    <div
+        data-testid="page-error"
+        class="flex flex-col gap-y-3 items-center justify-center p-6"
+    >
+        <img
+            v-if="$props.errorImage"
+            :src="$props.errorImage"
+            draggable="false"
+            class="max-w-md select-none w-full"
+        />
+        <IconMDIErrorOutline v-else :size="64" />
 
         <div
             role="alert"
             class="text-center"
-        >{{ message }}</div>
+        >
+            {{ $props.errorMessage || errorMessage }}
+        </div>
 
         <Button
-            v-if="errorCode === 401"
+            v-if="$props.errorCode === 401"
             @click="toLoginPage"
-            level="neutral"
-            class="border-stone-600/80! rounded-full!"
+            test-id="page-error-login-button"
+            class="border-stone-300/60! rounded-full!
+            md:border-stone-600/60!"
         >Log in</Button>
         <Button
-            v-else-if="errorCode !== 403 && errorCode !== 404"
+            v-else-if="$props.errorCode !== 403 && $props.errorCode !== 404"
             @click="$emit('retry')"
-            class="border-stone-600/80! rounded-full!"
+            test-id="page-error-retry-button"
+            class="border-stone-300/60! rounded-full!
+            md:border-stone-600/60!"
         >Try again</Button>
     </div>
 </template>
