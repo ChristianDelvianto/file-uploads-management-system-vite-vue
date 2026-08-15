416c757d8aac26cef927a3157999ef28da86dacf
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-08-15 19:16:55 +0700

    update PageLoading component; add slot default for flexibility

--- src/components/page/PageLoading.vue
+++ src/components/page/PageLoading.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
-import IconMDILoading from '@src/components/svg/mdi/Loading.vue'
+import IconMDILoading from '@/components/svg/mdi/Loading.vue'
+
+withDefaults(defineProps<Readonly<{
+    iconColor?: string,
+}>>(), {
+    iconColor: '#000'
+})
 </script>
 
 <template>
-    <div class="flex items-center justify-center p-6">
-        <IconMDILoading
-            color="#000"
-            :size="64"
-        />
+    <slot
+        v-if="$slots.default"
+        data-testid="page-loading-slot"
+    />
+    <div
+        v-else
+        data-testid="page-loading-default"
+        class="flex items-center justify-center p-6"
+    >
+        <IconMDILoading :color="$props.iconColor" :size="64" />
     </div>
 </template>
