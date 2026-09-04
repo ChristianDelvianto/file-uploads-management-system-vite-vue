965ababb1de4995ea6a013f7412bab23a14de6fa
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-09-04 11:27:19 +0700

    refactor CircleOutline component; simplify computed properties and remove unused watch

--- src/components/svg/custom/CircleOutline.vue
+++ src/components/svg/custom/CircleOutline.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, watch } from 'vue'
+import { computed } from 'vue'
 
 const props = withDefaults(defineProps<Readonly<{
     backgroundColor?: string,
@@ -21,21 +21,22 @@ const props = withDefaults(defineProps<Readonly<{
     value: 0,
 })
 
-const ceiledValue = computed<number>(function () {
+const ceiledValue = computed<number>(() => {
     return Math.ceil(props.value)
 })
-const circumference = computed<number>(function () {
-    return (props.radius - props.stroke * 2) * 2 * Math.PI
+const circumference = computed<number>(() => {
+    return (props.radius - props.stroke * 2)
+        * 2
+        * Math.PI
 })
-const normalisedRadius = computed<number>(function () {
-    return props.radius - props.stroke * 2
+const normalisedRadius = computed<number>(() => {
+    return props.radius - props.stroke
+        * 2
 })
-const strokeDashOffset = computed<number>(function () {
-    return (circumference.value - ceiledValue.value / 100 * circumference.value)
-});
-
-watch(ceiledValue, function (newValue): void {
-    console.log('ceiledValue newValue :', newValue)
+const strokeDashOffset = computed<number>(() => {
+    return circumference.value - ceiledValue.value
+        / 100
+        * circumference.value
 })
 </script>
 
@@ -43,11 +44,33 @@ watch(ceiledValue, function (newValue): void {
     <svg
         :height="$props.size"
         :width="$props.size"
+        xmlns="http://www.w3.org/2000/svg"
+        class="flex items-center justify-center relative"
     >
+        <!--
+            Background (#D1D5DC = text-gray-300)
+        -->
         <circle
             :cx="$props.cxy"
             :cy="$props.cxy"
-            :fill="$props.backgroundColor"
+            fill="#FFF"
+            :r="normalisedRadius"
+            stroke="#D1D5DC"
+            :stroke-dasharray="circumference + ' ' + circumference"
+            :stroke-dashoffset="100"
+            :stroke-width="$props.strokeWidth"
+            :style="{
+                transform: 'rotate(-90deg)',
+                transformOrigin: '50% 50%'
+            }"
+            class="absolute duration-300 ease-in-out transition-all z-[0]"
+        ></circle>
+
+        <!-- The progressive circle -->
+        <circle
+            :cx="$props.cxy"
+            :cy="$props.cxy"
+            fill="#FFF"
             :r="normalisedRadius"
             :stroke="$props.color"
             :stroke-dasharray="circumference + ' ' + circumference"
@@ -57,7 +80,7 @@ watch(ceiledValue, function (newValue): void {
                 transform: 'rotate(-90deg)',
                 transformOrigin: '50% 50%'
             }"
-            class="duration-300 ease-in-out transition-all"
+            class="duration-300 ease-in-out relative transition-all z-1"
         ></circle>
     </svg>
 </template>
