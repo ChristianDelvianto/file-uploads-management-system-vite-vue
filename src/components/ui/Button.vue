7a52c331516f705852cd306c64392f6f749a1997
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-09-04 11:56:15 +0700

    refactor Button component: streamline event emission and improve prop handling

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -1,48 +1,56 @@
 <script setup lang="ts">
-defineEmits<{
-    (eventName: 'click'): void,
+const emit = defineEmits<{
+    (eventName: 'click'): void
 }>()
 
 const props = withDefaults(defineProps<Readonly<{
     disabled?: boolean,
     level?: 'danger' | 'neutral' | 'primary',
-    type?: 'button' | 'submit',
+    testId?: string,
+    type?: 'button' | 'submit'
 }>>(), {
     disabled: false,
     level: 'neutral',
-    type: 'button',
+    type: 'button'
 })
 
 const backgroundColor = {
     'danger': 'bg-red-600',
     'neutral': '',
-    'primary': 'bg-blue-600',
+    'primary': 'bg-blue-600'
 }[props.level]
 const borderColor = {
     'danger': 'border-red-600',
     'neutral': 'border-stone-300',
-    'primary': 'border-blue-600',
+    'primary': 'border-blue-600'
 }[props.level]
 const focusEffect = {
     'danger': 'focus:bg-red-900 focus:outline-red-300',
     'neutral': 'focus:border-blue-600 focus:text-blue-600 focus:outline-blue-300',
-    'primary': 'focus:bg-blue-900 focus:outline-blue-300',
+    'primary': 'focus:bg-blue-900 focus:outline-blue-300'
 }[props.level]
 const hoverEffect = {
     'danger': 'hover:bg-red-900',
     'neutral': 'hover:bg-blue-100 hover:border-blue-600 hover:text-blue-600',
-    'primary': 'hover:bg-blue-900',
+    'primary': 'hover:bg-blue-900'
 }[props.level]
 const textColor = {
     'danger': 'text-white',
     'neutral': '',
-    'primary': 'text-white',
+    'primary': 'text-white'
 }[props.level]
+
+function handleClick(): void {
+    if (!props.disabled) {
+        emit('click')
+    }
+}
 </script>
 
 <template>
     <button
-        @click="$emit('click')"
+        @click="handleClick"
+        :data-testid="$props.testId"
         :type="$props.type"
         :disabled="$props.disabled"
         :class="[
