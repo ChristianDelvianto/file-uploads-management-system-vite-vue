2d7e2e766dc8899909b56a1f3d181088943dabfd
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-09-04 12:13:45 +0700

    add window.ts: define constants for Tailwindcss default widths

---
+++ src/const/window.ts
@@ -0,0 +1,11 @@
+/**
+ * Following Tailwindcss's default width
+ */
+
+export const MIN_WIDTH_SM: number = 640
+
+export const MIN_WIDTH_MD: number = 768
+
+export const MIN_WIDTH_LG: number = 1024
+
+export const MIN_WIDTH_XL: number = 1280
