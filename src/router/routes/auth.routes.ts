8607f5793d7442accd1a3a389aa75ed20d124f88
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-09-05 18:03:32 +0700

    refactor auth.routes.ts: restructure authentication routes for clarity and maintainability

--- src/router/routes/auth.routes.ts
+++ src/router/routes/auth.routes.ts
@@ -1,7 +1,30 @@
+import LoginPage from '@/pages/Login.vue'
+import SignupPage from '@/pages/Signup.vue'
 import type { RouteRecordRaw } from 'vue-router'
 
-const authRoutes: RouteRecordRaw[] = [
+const login: RouteRecordRaw = {
+    meta: {
+        guestOnly: true,
+        userOnly: false,
+        role: null,
+    },
+    path: '/login',
+    name: 'login',
+    component: LoginPage
+}
 
+const signup: RouteRecordRaw = {
+    meta: {
+        guestOnly: true,
+        userOnly: false,
+        role: null,
+    },
+    path: '/signup',
+    name: 'signup',
+    component: SignupPage
+}
+
+export default [
+    login,
+    signup
 ]
-
-export default authRoutes
