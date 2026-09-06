68004e62be8ebef6703194a761056e5ad3a7ee37
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-09-06 13:48:38 +0700

    refactor window module: simplify export structure for improved clarity

--- src/stores/modules/window/index.ts
+++ src/stores/modules/window/index.ts
@@ -2,15 +2,11 @@ import getters from './window.getters'
 import state from './window.state'
 import mutations from './window.mutations'
 import actions from './window.actions'
-import { Module } from 'vuex/types/index.js'
-import { StoreState } from './types'
 
-const windowModule: Module<StoreState, {}> = {
+export default {
     namespaced: true,
     getters,
     state,
     mutations,
-    actions,
+    actions
 }
-
-export default windowModule
