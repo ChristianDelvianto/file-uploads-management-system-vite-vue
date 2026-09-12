db850a890be6d1280efd70dd9f40d20f96cbd6cc
ChristianDelvianto <christiandelvianto1@gmail.com>
2026-09-12 14:52:25 +0700

    refactor service index.ts: remove token configuration

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,45 +1,18 @@
-import { TOKEN_KEY } from '@/const/localStorage'
-import axios, { AxiosError } from 'axios'
+import axios from 'axios'
 
 const Api = axios.create({
-    // baseURL: '', // Base URL already defined from src/main.ts
+    // Base URL already defined from src/main.ts
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
     },
-    withCredentials: false,
-    withXSRFToken: false
+    withCredentials: true,
+    withXSRFToken: true
 })
 
 /**
- * Request interceptors
+ * Since we are using cookie-based authentication,
+ * no need to configure axios interceptors.
  */
-Api.interceptors.request.use(
-    (req) => {
-        const token = localStorage.getItem(TOKEN_KEY)
-
-        if (token) {
-            req.headers['Authorization'] = `Bearer ${token}`
-        }
-
-        return req
-    },
-    (err) => {
-        return Promise.reject(err)
-    }
-)
-
-/**
- * Response interceptors
- */
-Api.interceptors.response.use(
-    (res) => res,
-    async (originalErr: AxiosError): Promise<never> => {
-        // No need for refresh token logic, we are using Laravel Sanctum
-        // (Unless we set token expiration from the backend)
-        
-        return Promise.reject(originalErr)
-    }
-)
 
 export default Api
